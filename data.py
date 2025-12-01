1745a1d912753ebcd340848a277875e3388599cd
NAHUEL CAERO <nc7519@mera.latam>
2025-12-01 12:46:12 -0300

    Desplegando proyecto beta

---
+++ data.py
@@ -0,0 +1,24 @@
+# Diccionario con tu información
+portfolio_data = {
+    "header": {
+        "name": "Caero Nahuel",
+        "title": "Junior Data Analyst",
+    },
+    "about": {
+        "description": "Junior Data Analyst enfocado en la transformación de datos transaccionales en estrategias de negocio accionables. Especializado en Python y en la creación de pipelines de análisis para optimizar la toma de decisiones basada en datos."
+    },
+    "projects": [
+        {
+            "id": 1,
+            "title": "Análisis de Cohortes E-commerce (RFM)",
+            "description": "Transformación de datos crudos de Olist (Brasil) en una estrategia de segmentación. Implementación de metodología RFM para identificar clientes VIP y reactivar usuarios en riesgo, solucionando el problema de marketing de 'talla única'.",
+            "stack": ["Python", "Pandas", "NumPy", "Matplotlib"],
+            "link": "https://github.com/Caero-Nahuel/Proyecto-analisis-de-cohortes-ecommerce"
+        }
+    ],
+    "contact": {
+        "email": "caero.nahuel@gmail.com",
+        "linkedin": "https://www.linkedin.com/in/nahuel-agustin-caero/",
+        "github": "https://github.com/Caero-Nahuel"
+    }
+}
